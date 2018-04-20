9c70b4602c67459c2c0e680825e285cac1a00d73
郑海波 <zhenghb@firstshare.cn>
2018-04-21 00:37:56 +0800

    spring boot

--- saas/hr-provider/src/main/java/com/nova/saas/hr/starter/AppStarter.java
+++ saas/hr-provider/src/main/java/com/nova/saas/hr/starter/AppStarter.java
@@ -2,12 +2,14 @@ package com.nova.saas.hr.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableAsync
-@SpringBootApplication
 @EnableTransactionManagement
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class})
 //public class AppStarter extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
 public class AppStarter {
     //    public static void main(String[] args) {
