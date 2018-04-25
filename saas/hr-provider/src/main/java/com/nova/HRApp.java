4f43f66f18c30fe52b8ff4ddf8c641b68792e336
郑海波 <zhenghb@firstshare.cn>
2018-04-25 12:51:05 +0800

    auth

--- saas/hr-provider/src/main/java/com/nova/HRApp.java
+++ saas/hr-provider/src/main/java/com/nova/HRApp.java
@@ -1,29 +1,22 @@
 package com.nova;
 
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.nova.paas.common.support.NovaBeanNameGenerator;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.system.ApplicationPidFileWriter;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @EnableAsync
 @EnableTransactionManagement
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class})
 @MapperScan("com.nova.paas.auth.mapper")
 //@ComponentScan(basePackages = {"com.nova"})
-public class HRApp extends WebMvcConfigurerAdapter {
+public class HRApp extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication webApp = new SpringApplication(HRApp.class);
         webApp.addListeners(new ApplicationPidFileWriter());
@@ -32,15 +25,28 @@ public class HRApp extends WebMvcConfigurerAdapter {
     }
 
     @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-        //处理中文乱码问题
-        List<MediaType> fastMediaTypes = new ArrayList<>();
-        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
-        fastConverter.setSupportedMediaTypes(fastMediaTypes);
-        fastConverter.setFastJsonConfig(fastJsonConfig);
-        converters.add(fastConverter);
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HRApp.class);
     }
 }
+//public class HRApp extends WebMvcConfigurerAdapter {
+//    public static void main(String[] args) {
+//        SpringApplication webApp = new SpringApplication(HRApp.class);
+//        webApp.addListeners(new ApplicationPidFileWriter());
+//        webApp.setBeanNameGenerator(new NovaBeanNameGenerator());
+//        webApp.run(args);
+//    }
+//
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+//        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+//        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+//        //处理中文乱码问题
+//        List<MediaType> fastMediaTypes = new ArrayList<>();
+//        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+//        fastConverter.setSupportedMediaTypes(fastMediaTypes);
+//        fastConverter.setFastJsonConfig(fastJsonConfig);
+//        converters.add(fastConverter);
+//    }
+//}
