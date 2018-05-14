3cfbb8b8b2d9cf8569bdd9db88efa896dcd52e92
silent-sky <39961939@qq.com>
2018-05-14 18:28:07 +0800

    mapper

--- saas/hr-provider/src/main/java/com/nova/HRApp.java
+++ saas/hr-provider/src/main/java/com/nova/HRApp.java
@@ -4,7 +4,6 @@ import com.nova.paas.common.support.NovaBeanNameGenerator;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -14,8 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableAsync
 @EnableTransactionManagement
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
-//@MapperScan("com.nova.paas.auth.mapper")
+@SpringBootApplication
 @ComponentScan(basePackages = {"com.nova"})
 @MapperScan(basePackages = "com.nova.**.mapper")
 public class HRApp extends SpringBootServletInitializer {
@@ -31,24 +29,3 @@ public class HRApp extends SpringBootServletInitializer {
         return application.sources(HRApp.class);
     }
 }
-//public class HRApp extends WebMvcConfigurerAdapter {
-//    public static void main(String[] args) {
-//        SpringApplication webApp = new SpringApplication(HRApp.class);
-//        webApp.addListeners(new ApplicationPidFileWriter());
-//        webApp.setBeanNameGenerator(new NovaBeanNameGenerator());
-//        webApp.run(args);
-//    }
-//
-//    @Override
-//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-//        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-//        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-//        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-//        //处理中文乱码问题
-//        List<MediaType> fastMediaTypes = new ArrayList<>();
-//        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
-//        fastConverter.setSupportedMediaTypes(fastMediaTypes);
-//        fastConverter.setFastJsonConfig(fastJsonConfig);
-//        converters.add(fastConverter);
-//    }
-//}
